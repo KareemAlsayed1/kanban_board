d000739825d38d5b4cd2913a2e0dd6e6940befd6
Kareem Alsayed <Kareem@minerva.kgi.edu>
2020-03-16 22:19:07 +0530

    fixing a bug in the signup

--- app/auth.py
+++ app/auth.py
@@ -17,7 +17,7 @@ def signup():
 
 @auth.route('/signup', methods=['POST'])
 def signup_post():
-    email = request.form.get('email')
+    email = request.form.get('email').lower()
     first_name = request.form.get('first_name')
     last_name = request.form.get('last_name')
     password = request.form.get('pwd1')
